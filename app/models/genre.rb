d6806ad6bba3fea3dfd609925030fda86059a8ec
Austin Cummings <aac729@live.com>
2020-09-04 17:25:09 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artists = self.artist.collect do |artist|
+    artists = self.artists.collect do |artist|
       artist.name 
     end
   end
